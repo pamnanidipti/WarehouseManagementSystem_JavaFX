1e793f7f234b2420891e96fd3bd3772c11e4fe26
Dipti Pamnani <pamnanidipti@gmail.com>
2017-04-29 02:38:11 -0400

    addadmin

--- src/controllers/AdminController.java
+++ src/controllers/AdminController.java
@@ -1,6 +1,5 @@
 package controllers;
 
-
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -8,7 +7,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import javafx.application.Platform;
 import javafx.scene.Node;
-
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -23,152 +21,155 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import models.Connector;
 
-
 /**
- * @author Aakash Tyagi
- * Date : 20 April 2017
+ * @author Aakash Tyagi Date : 20 April 2017
  * @param args the command line arguments
- * @throws InterruptedException 
+ * @throws InterruptedException
  */
-
 public class AdminController {
-	  public static String sessionUser = null;
-          public boolean isEdit;
-          private static int count;
-          
-	 @FXML
-	    private TextField AdmintfFirstName;
-	 @FXML
-	    private TextField AdmintfLastName;
-	@FXML
-	    private TextField AdmintfID;
-	 @FXML
-	    private TextField AdmintfEmailID; 
-         @FXML
-         private Button updateProfileButton;
+
+    public static String sessionUser = null;
+    public boolean isEdit;
+
+    @FXML
+    private TextField AdmintfFirstName;
+    @FXML
+    private TextField AdmintfLastName;
+    @FXML
+    private TextField AdmintfID;
+    @FXML
+    private TextField AdmintfEmailID;
+    @FXML
+    private Button updateProfileButton;
     @FXML
     private PasswordField AdminpfPassword;
     @FXML
     private TextField AdmintfUserName;
     @FXML
     private TextField AdmintfCity;
-    
-    private void setAllFieldDisableOnClick(){
-    	AdmintfFirstName.setDisable(true);
-    	AdmintfLastName.setDisable(true);
-    	AdmintfID.setDisable(true);
-    	AdmintfEmailID.setDisable(true);
-    	AdminpfPassword.setDisable(true);
-    	AdmintfUserName.setDisable(true);
-    	AdmintfCity.setDisable(true);
+
+    private void setAllFieldDisableOnClick() {
+        AdmintfFirstName.setDisable(true);
+        AdmintfLastName.setDisable(true);
+        AdmintfID.setDisable(true);
+        AdmintfEmailID.setDisable(true);
+        AdminpfPassword.setDisable(true);
+        AdmintfUserName.setDisable(true);
+        AdmintfCity.setDisable(true);
         updateProfileButton.setDisable(true);
     }
-    
-    private void setAllFieldEnableOnClick(){
-    	AdmintfFirstName.setDisable(false);
-    	AdmintfLastName.setDisable(false);
-    	AdmintfID.setDisable(false);
-    	AdmintfEmailID.setDisable(false);
-    	AdminpfPassword.setDisable(false);
-    	AdmintfUserName.setDisable(false);
-    	AdmintfCity.setDisable(false);
-       updateProfileButton.setDisable(false);
+
+    private void setAllFieldEnableOnClick() {
+        AdmintfFirstName.setDisable(false);
+        AdmintfLastName.setDisable(false);
+        //AdmintfID.setDisable(false);
+        AdmintfEmailID.setDisable(false);
+        AdminpfPassword.setDisable(false);
+        AdmintfUserName.setDisable(false);
+        AdmintfCity.setDisable(false);
+        updateProfileButton.setDisable(false);
     }
-    
-    private void setAllFieldClearOnClick(){
-    	AdmintfFirstName.clear();
-    	AdmintfLastName.clear();
-    	AdmintfID.clear();
-    	AdmintfEmailID.clear();
-    	AdminpfPassword.clear();
-    	AdmintfUserName.clear();
-    	AdmintfCity.clear();
-        
+
+    private void setAllFieldClearOnClick() {
+        AdmintfFirstName.clear();
+        AdmintfLastName.clear();
+        AdmintfID.clear();
+        AdmintfEmailID.clear();
+        AdminpfPassword.clear();
+        AdmintfUserName.clear();
+        AdmintfCity.clear();
+
     }
     static String ID;
-    
-    public void getAdminUserName(String ID){
-    	ID= AdmintfUserName.getText();
-    	
-    	setAdminUserName(ID);
+
+    public void getAdminUserName(String ID) {
+        ID = AdmintfUserName.getText();
+
+        setAdminUserName(ID);
     }
-    
-    public void setAdminUserName(String ID){
+
+    public void setAdminUserName(String ID) {
         this.ID = ID;
     }
-    
-    private  Connector conn = new Connector();
+
+    private Connector conn = new Connector();
     private Connection connection;
-	private Statement statement;
-	private ResultSet rslt;
-        
+    private Statement statement;
+    private ResultSet rslt;
+
     @FXML
-    private void setAdminEditProfileClick(Event event){
-    	setAllFieldEnableOnClick();
+    private void setAdminEditProfileClick(Event event) {
+        setAllFieldEnableOnClick();
         isEdit = true;
-        String sqlQuery = "select * FROM admin where adminUserName ='"+sessionUser+"'; ";
+        String sqlQuery = "select * FROM admin where adminUserName ='" + sessionUser + "'; ";
         try {
-            
+
             connection = conn.connect();
             statement = connection.createStatement();
             rslt = statement.executeQuery(sqlQuery);
-            while(rslt.next()) {
-            	AdmintfFirstName.setText(rslt.getString("adminFirstname"));
-            	AdmintfLastName.setText(rslt.getString("adminLastname"));
-            	AdmintfID.setText(rslt.getString("adminId"));
-            	AdmintfEmailID.setText(rslt.getString("adminEmailId"));
-            	AdminpfPassword.setText(rslt.getString("adminPassword"));
-            	AdmintfUserName.setText(rslt.getString("adminUserName"));
-            	AdmintfCity.setText(rslt.getString("adminCity"));
+            while (rslt.next()) {
+                AdmintfFirstName.setText(rslt.getString("adminFirstname"));
+                AdmintfLastName.setText(rslt.getString("adminLastname"));
+                AdmintfID.setText(rslt.getString("adminId"));
+                AdmintfEmailID.setText(rslt.getString("adminEmailId"));
+                AdminpfPassword.setText(rslt.getString("adminPassword"));
+                AdmintfUserName.setText(rslt.getString("adminUserName"));
+                AdmintfCity.setText(rslt.getString("adminCity"));
             }
-          
+            connection.close();
+            statement.close();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
     }
+
     @FXML
-    private void setAdminUpdateProfileClick(Event event){
-        
+    private void setAdminUpdateProfileClick(Event event) {
+
         try {
-            if(isEdit){
-               connection = conn.connect();
-            statement = connection.createStatement();
-            
-            ResultSet resultSet = statement.executeQuery("select count(*) from admin");
-            while (resultSet.next()) {
-            count = resultSet.getInt(1);
- }
-               // System.out.println("Count: "+count);
-            //statement.executeUpdate("insert into admin ")
+            if (isEdit) {
+                connection = conn.connect();
+                statement = connection.createStatement();
+
+               
+                // System.out.println("Count: "+count);
+                statement.executeUpdate("update admin set adminFirstName ='" + AdmintfFirstName.getText() + "',adminLastName='" + AdmintfLastName.getText() + "',adminUserName ='" + AdmintfUserName.getText() + "',adminPassword ='" + AdminpfPassword.getText() + "',adminEmailId ='" + AdmintfEmailID.getText() + "',adminCity='" + AdmintfCity.getText() + "' where adminId=" + AdmintfID.getText() + ";");
+                System.out.println("Your Details have been updated successfully!");
+                setAllFieldDisableOnClick();
+                connection.close();
+                statement.close();
+                
             }
-          
-        } catch(Exception e){
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @FXML
-    private void setAdminCancelButtonClick(Event event){
-    	setAllFieldDisableOnClick();
+    private void setAdminCancelButtonClick(Event event) {
+        setAllFieldDisableOnClick();
         setAllFieldClearOnClick();
     }
     /*@FXML
-    private void setAdminCoursePanelClick(Event event) throws IOException {
+     private void setAdminCoursePanelClick(Event event) throws IOException {
 
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("/admin/Course.fxml"));
-        loader.load();
-        Parent p = loader.getRoot();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(p));
-        stage.setTitle("Course Panel");
-        stage.show();
-    }*/
+     FXMLLoader loader = new FXMLLoader();
+     loader.setLocation(getClass().getResource("/admin/Course.fxml"));
+     loader.load();
+     Parent p = loader.getRoot();
+     Stage stage = new Stage();
+     stage.setScene(new Scene(p));
+     stage.setTitle("Course Panel");
+     stage.show();
+     }*/
+
     @FXML
     private void setAdminAddAdminClick(Event event) throws IOException {
 
-        FXMLLoader fxload= new FXMLLoader();
+        FXMLLoader fxload = new FXMLLoader();
         fxload.setLocation(getClass().getResource("/views/AddAdmin.fxml"));
         fxload.load();
         Parent parent = fxload.getRoot();
