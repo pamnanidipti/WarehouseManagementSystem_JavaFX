5f1ac16237b461c84f78649d6a4c827b5c8b9793
Dipti Pamnani <pamnanidipti@gmail.com>
2017-04-30 12:57:28 -0400

    table corrected

--- src/controllers/AddAdminController.java
+++ src/controllers/AddAdminController.java
@@ -1,14 +1,15 @@
 package controllers;
 
-
 import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.scene.Node;
-
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -25,147 +26,145 @@ import javafx.stage.Stage;
 import models.AdminPOJOTable;
 import models.Connector;
 
-
 /**
- * @author Aakash Tyagi
- * Date : 20 April 2017
+ * @author Aakash Tyagi Date : 20 April 2017
  * @param args the command line arguments
- * @throws InterruptedException 
+ * @throws InterruptedException
  */
+public class AddAdminController implements Initializable {
 
-public class AddAdminController {
-	  public static String sessionUser = null;
-          private static int count;
-          
-	 @FXML
-	    private TextField addAdmintfFirstName;
-	 @FXML
-	    private TextField addAdmintfLastName;
-	@FXML
-	    private TextField addAdmintfID;
-	 @FXML
-	    private TextField addAdmintfEmailID; 
+    public static String sessionUser = null;
+    private static int count;
+
+    @FXML
+    private TextField addAdmintfFirstName;
+    @FXML
+    private TextField addAdmintfLastName;
+    @FXML
+    private TextField addAdmintfID;
+    @FXML
+    private TextField addAdmintfEmailID;
     @FXML
     private PasswordField addAdminpfPassword;
     @FXML
     private TextField addAdmintfUserName;
     @FXML
     private TextField addAdmintfCity;
-    
+
     @FXML
     private TableView<AdminPOJOTable> adminDetailsTableView;
-    
+
     @FXML
-    TableColumn<AdminPOJOTable,Integer> admindetailsId;
+    TableColumn<AdminPOJOTable, Integer> admindetailsId;
     @FXML
-    TableColumn<AdminPOJOTable,String> admindetailsFname;
+    TableColumn<AdminPOJOTable, String> admindetailsFname;
     @FXML
-    TableColumn<AdminPOJOTable,String> admindetailsLname;
+    TableColumn<AdminPOJOTable, String> admindetailsLname;
     @FXML
-    TableColumn<AdminPOJOTable,String> admindetailscity;
-    
+    TableColumn<AdminPOJOTable, String> admindetailscity;
+
     @FXML
     private Button save;
-    private void setAllFieldDisableOnClick(){
-    	addAdmintfFirstName.setDisable(true);
-    	addAdmintfLastName.setDisable(true);
-    	addAdmintfID.setDisable(true);
-    	addAdmintfEmailID.setDisable(true);
-    	addAdminpfPassword.setDisable(true);
-    	addAdmintfUserName.setDisable(true);
-    	addAdmintfCity.setDisable(true);
+
+    private void setAllFieldDisableOnClick() {
+        addAdmintfFirstName.setDisable(true);
+        addAdmintfLastName.setDisable(true);
+        addAdmintfID.setDisable(true);
+        addAdmintfEmailID.setDisable(true);
+        addAdminpfPassword.setDisable(true);
+        addAdmintfUserName.setDisable(true);
+        addAdmintfCity.setDisable(true);
     }
-    
-    private void setAllFieldEnableOnClick(){
-    	addAdmintfFirstName.setDisable(false);
-    	addAdmintfLastName.setDisable(false);
-    	//addAdmintfID.setDisable(false);
-    	addAdmintfEmailID.setDisable(false);
-    	addAdminpfPassword.setDisable(false);
-    	addAdmintfUserName.setDisable(false);
-    	addAdmintfCity.setDisable(false);
+
+    private void setAllFieldEnableOnClick() {
+        addAdmintfFirstName.setDisable(false);
+        addAdmintfLastName.setDisable(false);
+        //addAdmintfID.setDisable(false);
+        addAdmintfEmailID.setDisable(false);
+        addAdminpfPassword.setDisable(false);
+        addAdmintfUserName.setDisable(false);
+        addAdmintfCity.setDisable(false);
     }
-    
-    private void setAllFieldClearOnClick(){
-    	addAdmintfFirstName.clear();
-    	addAdmintfLastName.clear();
-    	addAdmintfID.clear();
-    	addAdmintfEmailID.clear();
-    	addAdminpfPassword.clear();
-    	addAdmintfUserName.clear();
-    	addAdmintfCity.clear();
-        
+
+    private void setAllFieldClearOnClick() {
+        addAdmintfFirstName.clear();
+        addAdmintfLastName.clear();
+        addAdmintfID.clear();
+        addAdmintfEmailID.clear();
+        addAdminpfPassword.clear();
+        addAdmintfUserName.clear();
+        addAdmintfCity.clear();
+
     }
-   
-    
-    private  Connector conn = new Connector();
+
+    private Connector conn = new Connector();
     private Connection connection;
-	private Statement statement;
-	//private ResultSet rslt;
-	@FXML
-	private Label addAdminSaveLabel;
+    private Statement statement;
+    //private ResultSet rslt;
     @FXML
-    private void setAddAdminSaveClick(Event event){
-    	setAllFieldEnableOnClick();
-    	try {
-    		  		
-    		connection=conn.connect();
-    		statement = connection.createStatement();
-                
-                 ResultSet resultSet = statement.executeQuery("select count(*) from admin");
-                while (resultSet.next()) {
-                    count = resultSet.getInt(1);
-                }
-                
-                addAdmintfID.setText(String.valueOf(count+1));
-                if(addAdmintfFirstName.getText().equals("") || addAdmintfLastName.getText().equals("") || addAdmintfEmailID.getText().equals("")
-                        || addAdminpfPassword.getText().equals("") || addAdmintfUserName.getText().equals("") || addAdmintfCity.getText().equals("")){
-                    //save.setDisable(true);
-                    addAdminSaveLabel.setTextFill(Color.web("red"));
-                    addAdminSaveLabel.setText("Please enter all values!");
-                    
-                    return;
-                }
-                else{
-                    //save.setDisable(false);
-        String sqlQuery = "insert into admin (adminId,adminFirstName,adminLastName,adminUserName,adminPassword,adminEmailId,adminCity )"+"values ("+addAdmintfID.getText() +" , '"+addAdmintfFirstName.getText()+"','"+addAdmintfLastName.getText()+"','"+addAdmintfUserName.getText()+"','"+addAdminpfPassword.getText()+"','"+addAdmintfEmailID.getText()+"','"+addAdmintfCity.getText()+"')";
-        //System.out.println(sqlQuery);
-        statement.executeUpdate(sqlQuery);
-        addAdminSaveLabel.setTextFill(Color.web("green"));
-        addAdminSaveLabel.setText("Admin Saved to Database");
-        setAllFieldClearOnClick();
-        setAllFieldDisableOnClick();
-                }
-        connection.close();
-        statement.close();
-        resultSet.close();
-            
-            }catch (SQLException e) {
-            	addAdminSaveLabel.setText("Admin Not Saved to Database");
-            	System.out.println("Values are not inserted in admin table after save button");
-                e.printStackTrace();}
-            }
-          
-  /* 
+    private Label addAdminSaveLabel;
+
     @FXML
-    private void setAddAdminSaveClick(Event event){
-        //String sqlQuery = "select * FROM admin where adminUserName ='"+sessionUser+"'; ";
+    private void setAddAdminSaveClick(Event event) {
+        setAllFieldEnableOnClick();
         try {
-            
-          
-        } catch(Exception e){
+
+            connection = conn.connect();
+            statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select count(*) from admin");
+            while (resultSet.next()) {
+                count = resultSet.getInt(1);
+            }
+
+            addAdmintfID.setText(String.valueOf(count + 1));
+            if (addAdmintfFirstName.getText().equals("") || addAdmintfLastName.getText().equals("") || addAdmintfEmailID.getText().equals("")
+                    || addAdminpfPassword.getText().equals("") || addAdmintfUserName.getText().equals("") || addAdmintfCity.getText().equals("")) {
+                //save.setDisable(true);
+                addAdminSaveLabel.setTextFill(Color.web("red"));
+                addAdminSaveLabel.setText("Please enter all values!");
+
+                return;
+            } else {
+                //save.setDisable(false);
+                String sqlQuery = "insert into admin (adminId,adminFirstName,adminLastName,adminUserName,adminPassword,adminEmailId,adminCity )" + "values (" + addAdmintfID.getText() + " , '" + addAdmintfFirstName.getText() + "','" + addAdmintfLastName.getText() + "','" + addAdmintfUserName.getText() + "','" + addAdminpfPassword.getText() + "','" + addAdmintfEmailID.getText() + "','" + addAdmintfCity.getText() + "')";
+                //System.out.println(sqlQuery);
+                statement.executeUpdate(sqlQuery);
+                addAdminSaveLabel.setTextFill(Color.web("green"));
+                addAdminSaveLabel.setText("Admin Saved to Database");
+                setAllFieldClearOnClick();
+                setAllFieldDisableOnClick();
+            }
+            connection.close();
+            statement.close();
+            resultSet.close();
+
+        } catch (SQLException e) {
+            addAdminSaveLabel.setText("Admin Not Saved to Database");
+            System.out.println("Values are not inserted in admin table after save button");
             e.printStackTrace();
         }
-    }*/
-    
+    }
+
+    /* 
+     @FXML
+     private void setAddAdminSaveClick(Event event){
+     //String sqlQuery = "select * FROM admin where adminUserName ='"+sessionUser+"'; ";
+     try {
+            
+          
+     } catch(Exception e){
+     e.printStackTrace();
+     }
+     }*/
     @FXML
-    private void setLogoutButtonClick(Event event) throws IOException{
-        
+    private void setLogoutButtonClick(Event event) throws IOException {
+
         FXMLLoader fxload = new FXMLLoader();
         fxload.setLocation(getClass().getResource("/application/Login.fxml"));
         fxload.load();
         Parent parent = fxload.getRoot();
-        ((Node)event.getSource()).getScene().getWindow().hide();
+        ((Node) event.getSource()).getScene().getWindow().hide();
         Stage stage = new Stage();
         stage.setScene(new Scene(parent));
         stage.setTitle("Login Page - Warehouse Management System");
@@ -173,87 +172,76 @@ public class AddAdminController {
     }
 
     @FXML
-    private void setaddAdminCancelButton(Event event){
-    	setAllFieldDisableOnClick();
+    private void setaddAdminCancelButton(Event event) {
+        setAllFieldDisableOnClick();
         setAllFieldClearOnClick();
         addAdminSaveLabel.setText("");
     }
     /*@FXML
-    private void setbuttonClick(Event event) throws IOException {
+     private void setbuttonClick(Event event) throws IOException {
 
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("/admin/Course.fxml"));
-        loader.load();
-        Parent p = loader.getRoot();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(p));
-        stage.setTitle("Course Panel");
-        stage.show();
-    }*/
-    @FXML
-    void initialize(){
-         assert adminDetailsTableView != null : "fx:id=\"tableview\" was not injected: check your FXML file 'UserMaster.fxml'.";
-    	
-         admindetailsFname.setCellValueFactory(
-            new PropertyValueFactory<AdminPOJOTable,String>("FName"));        
-         admindetailsLname.setCellValueFactory(                
-            new PropertyValueFactory<AdminPOJOTable,String>("LName"));
-         admindetailsId.setCellValueFactory(                
-                new PropertyValueFactory<AdminPOJOTable,Integer>("adminID"));
-         admindetailscity.setCellValueFactory(
-            new PropertyValueFactory<AdminPOJOTable,String>("city"));   
-         Connector connector = new Connector();
-       
-    
-        
-        try{
-        	 Connection conn = connector.connect();
-        	   
+     FXMLLoader loader = new FXMLLoader();
+     loader.setLocation(getClass().getResource("/admin/Course.fxml"));
+     loader.load();
+     Parent p = loader.getRoot();
+     Stage stage = new Stage();
+     stage.setScene(new Scene(p));
+     stage.setTitle("Course Panel");
+     stage.show();
+     }*/
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //assert adminDetailsTableView != null : "fx:id=\"tableview\" was not injected: check your FXML file 'UserMaster.fxml'.";
+        admindetailsId.setCellValueFactory(new PropertyValueFactory<AdminPOJOTable, Integer>("adminID"));
+        admindetailsFname.setCellValueFactory(new PropertyValueFactory<AdminPOJOTable, String>("FName"));
+        admindetailsLname.setCellValueFactory(new PropertyValueFactory<AdminPOJOTable, String>("LName"));
+        admindetailscity.setCellValueFactory(new PropertyValueFactory<AdminPOJOTable, String>("city"));
+         //Connector connector = new Connector();
+
+        try {
             buildData();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
     }
 
+    // private ObservableList<AdminPOJOTable> data = FXCollections.observableArrayList();
     private ObservableList<AdminPOJOTable> data;
 
-    public void buildData(){   
-        data = FXCollections.observableArrayList();
-       
-        try{    
-        	Connector connector = new Connector();
-       	 Connection conn = connector.connect();
-       	 Statement statement = null;
-            //String SQL = "Select adminId,adminFirstName,adminLastName,adminCity from admin;";            
-       	String SQL = "Select * from admin;";
-              
-            ResultSet rs= conn.createStatement().executeQuery(SQL);  
-            
-           
-            
-            while(rs.next()){
-                AdminPOJOTable cm = new AdminPOJOTable();
-                /*//Image img = new Image("tailoring/UserPhoto/User"+cm.getUserId().toString()+".jpg");                
+    //private ArrayList<AdminPOJOTable> data = new ArrayList<AdminPOJOTable>();
 
-               // ImageView mv = new ImageView();
-                mv.setImage(img);
-                mv.setFitWidth(70);
-                mv.setFitHeight(80);
-                cm.userPhoto.set(mv);*/
+    public void buildData() {
+        //data = FXCollections.observableArrayList();
+
+        try {
+            data = FXCollections.observableArrayList();
+            connection = conn.connect();
+            statement = connection.createStatement();
+                        
+            String SQL = "Select * from admin;";
+
+            ResultSet rs = statement.executeQuery(SQL);
+
+            while (rs.next()) {
+                AdminPOJOTable cm = new AdminPOJOTable();
+                //System.out.println("rs id"+rs.getInt("adminId"));
                 cm.adminID.set(rs.getInt("adminId"));
                 cm.FName.set(rs.getString("adminFirstName"));
                 cm.LName.set(rs.getString("adminLastName"));
-                  
                 cm.city.set(rs.getString("adminCity"));
-                data.add(cm);                  
+                data.add(cm);
             }
+
             adminDetailsTableView.setItems(data);
+            connection.close();
+            statement.close();
+            
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error on Building Data");
         }
-        catch(Exception e){
-              e.printStackTrace();
-              System.out.println("Error on Building Data");            
-      }
-   }
+    }
 }
--- src/models/AdminPOJOTable.java
+++ src/models/AdminPOJOTable.java
@@ -5,26 +5,51 @@ import javafx.beans.property.SimpleStringProperty;
 
 public class AdminPOJOTable {
 	
-	public SimpleIntegerProperty adminID = new SimpleIntegerProperty();
-	   public SimpleStringProperty FName = new SimpleStringProperty(); 
-	   public SimpleStringProperty LName = new SimpleStringProperty();
-	   public SimpleStringProperty city = new SimpleStringProperty();
-	  
-	   public Integer getadminID() {
-	      return adminID.get();
-	   }
+	public  SimpleIntegerProperty adminID = new SimpleIntegerProperty();
+	   public  SimpleStringProperty FName= new SimpleStringProperty();
+	   public  SimpleStringProperty LName= new SimpleStringProperty();
+	   public  SimpleStringProperty city= new SimpleStringProperty();
 
-	   public String getFName() {
-	      return FName.get();
-	   }
+    public AdminPOJOTable() {
+    }
 
-	   public String getLName() {
-	      return LName.get();
-	   }
+           
+//    public AdminPOJOTable(int adminID, String FName,String LName,String city ) {
+//        this.adminID = new SimpleIntegerProperty(adminID);
+//        this.FName = new SimpleStringProperty(FName);
+//        this.LName = new SimpleStringProperty(LName);
+//        this.city = new SimpleStringProperty(city);
+//        
+//    }
+    
+//    public void setAdminId(Integer adminID) {
+//        this.adminID.set(adminID);
+//    }
+//    public void setAdminFName(String adminFname) {
+//        this.FName.set(adminFname);
+//    }
+//    public void setAdminLName(String adminLname) {
+//        this.LName.set(adminLname);
+//    }
+//    public void setAdminCity(String city) {
+//        this.city.set(city);
+//    }
+    public int getAdminID() {
+        return adminID.getValue();
+    }
 
-	   public String getcity() {
-	      return city.get();
-	   }
+    public String getFName() {
+        return FName.getValue();
+    }
 
-	}
+    public String getLName() {
+        return LName.getValue();
+    }
+
+    public String getCity() {
+        return city.getValue();
+    }
+    
+
+    	}
 
